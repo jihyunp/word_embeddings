29a938c8cd3064b40aef1616e0dba9c801a0bb82
jihyunp <jihyunipark@gmail.com>
2016-08-11 17:43:22 -0700

    small change

--- word2vec_wmd.py
+++ word2vec_wmd.py
@@ -1,9 +1,10 @@
 from RedditData import RedditData, plot_score_histogram
 
 data_dir = './reddit_data_MH/2015'
-reddit_data = RedditData(data_dir)
-n_test = 500
+word2vec_file = './word2vec_reddit_2008_2015_20/gensim_word2vec_2008_2015_orig.vectors'
+reddit_data = RedditData(data_dir, word2vec_file)
 
+n_test = 500
 within_subreddit, within_post, random_doc = reddit_data.get_three_scores(n_test)
 
 plot_score_histogram(within_subreddit, 'within Subreddit', 'wmd_hist_within_subreddit.pdf')
