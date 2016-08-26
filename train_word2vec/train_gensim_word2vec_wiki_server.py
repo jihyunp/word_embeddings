99a35167b648bd62c3b97c10336c3207aab8b518
jihyunp <jihyunipark@gmail.com>
2016-08-26 09:50:48 -0700

    changed model variable name

--- train_word2vec/train_gensim_word2vec_wiki_server.py
+++ train_word2vec/train_gensim_word2vec_wiki_server.py
@@ -3,6 +3,6 @@ from WikiData import WikiData
 data_root_dir = '/extra/jihyunp0/research/word_embeddings/data'
 w2v_file = '/extra/jihyunp0/research/word_embeddings/result/wiki_word2vec_sg/wiki_word2vec_sg.vectors'
 
-billion_w2v = WikiData(data_dir=data_root_dir, word2vec_file=w2v_file, binary=False,
+wiki_w2v = WikiData(data_dir=data_root_dir, word2vec_file=w2v_file, binary=False,
                               vdim=200, window=5, sg=True, min_count=20, workers=8)
 
