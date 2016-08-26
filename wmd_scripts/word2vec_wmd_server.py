8ba63e516e70da1efba0e900f8a67e18a9497d4d
jihyunp <jihyunipark@gmail.com>
2016-08-26 10:29:33 -0700

    wmd related scripts in one place

--- wmd_scripts/word2vec_wmd_server.py
+++ wmd_scripts/word2vec_wmd_server.py
@@ -6,7 +6,7 @@ data_dir = '/extra/jihyunp0/research/word_embeddings/data/reddit_data_MH'
 word2vec_file = '/extra/jihyunp0/research/word_embeddings/result/gensim_word2vec_2008_2015.vectors'
 reddit_data = RedditData(data_dir, word2vec_file)
 
-n_test = 2000
+n_test = 1000
 within_subreddit_cbow, within_post_cbow, random_doc_cbow, comments_cbow = reddit_data.get_three_scores(n_test)
 
 plot_score_histogram(within_subreddit_cbow, 'within Subreddit', 'wmd_hist_within_subreddit.pdf')
@@ -21,7 +21,7 @@ plot_three_scores_hist([within_subreddit_cbow, within_post_cbow, random_doc_cbow
 word2vec_file = '/extra/jihyunp0/research/word_embeddings/result/reddit_word2vec_sg/gensim_word2vec_sg_2008_2015.vectors'
 reddit_data_sg = RedditData(data_dir, word2vec_file)
 
-n_test = 100
+n_test = 1000
 within_subreddit, within_post, random_doc, comments_sg = reddit_data_sg.get_three_scores(n_test)
 
 plot_score_histogram(within_subreddit, 'within Subreddit', 'wmd_hist_within_subreddit.pdf')
